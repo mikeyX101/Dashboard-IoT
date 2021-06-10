4f85323a28f306fbfd26b5ec46327ae8da195e44
Samuel Caron <samuel.caron101@gmail.com>
2021-06-10 19:37:03 -0400

    Address already in use and specified TLS1.2 for SSL Protocol

--- DashboardIoT/Program.cs
+++ DashboardIoT/Program.cs
@@ -24,12 +24,13 @@ namespace DashboardIoT
 				{
 					webBuilder.UseKestrel(options =>
 					{
-						options.Listen(IPAddress.Any, 5001, httpsOptions =>
+						options.Listen(IPAddress.Any, 5001, listenOptions =>
 						{
-							httpsOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
-							httpsOptions.UseHttps(System.Environment.GetEnvironmentVariable("IOT_HTTPS_CERT"), System.Environment.GetEnvironmentVariable("IOT_HTTPS_PASS"));
+							listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2;
+							
+							listenOptions.UseHttps(System.Environment.GetEnvironmentVariable("IOT_HTTPS_CERT"), System.Environment.GetEnvironmentVariable("IOT_HTTPS_PASS"), httpsOptions => httpsOptions.SslProtocols = System.Security.Authentication.SslProtocols.Tls12);
 						});
-						options.ListenAnyIP(8883, listenOptions => listenOptions.UseMqtt()); // 1883 for unsecured, 8883 for secured
+						//options.ListenAnyIP(8883, listenOptions => listenOptions.UseMqtt()); // 1883 for unsecured, 8883 for secured
 					});
 					webBuilder.UseStartup<Startup>();
 				});
